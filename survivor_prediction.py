1679869b57d0189324cb7b81c1188bc53954672a
Franco Sasieta <fsasieta@users.noreply.github.com>
2016-09-22 23:53:12 -0400

    Gender, class and ticket model. Only women survived for now, which didn't give me a great score

--- survivor_prediction.py
+++ survivor_prediction.py
@@ -11,9 +11,99 @@ as tuple with the header of the list
 For ease of tinkering, the original header is:
 Survived is a boolean
 
+    0               1           2       3       4       5       7       8       9       10          11      12
 ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
 
 """
+
+def gender_class_and_ticket_price(header, data):
+
+    fare_ceiling = 40
+    #any fare greater than 40 will equal 39 now
+    data[ data[0::,9].astype(np.float) >= fare_ceiling, 9] = fare_ceiling - 1.0
+
+    fare_bracket_size = 10
+    number_of_price_brackets = fare_ceiling / fare_bracket_size
+    number_of_classes = len(np.unique(data[0::2]))
+
+    survival_table = np.zeros((2, number_of_classes, number_of_price_brackets))
+
+    for passenger_class in xrange(number_of_classes):
+        for price_bracket in xrange(number_of_price_brackets):
+
+            #creates two array of booleans
+            women_only_stats = data[ (data[0::,4] == "female") \
+                            #select only women
+                        &   (data[0::,2].astype(np.float) == passenger_class  + 1) \
+                            #of a specific passenger class
+                        &   (data[0::,9].astype(np.float) >= price_bracket * fare_bracket_size) \
+                            #make sure theyre ticket range
+                        &   (data[0::,9].astype(np.float) <  (price_bracket + 1) * fare_bracket_size),1]
+                            #put in the second column
+
+            men_only_stats = data[ (data[0::,4] == "female") \
+                            #select only men
+                        &   (data[0::,2].astype(np.float) == passenger_class  + 1) \
+                            #of a specific passenger class
+                        &   (data[0::,9].astype(np.float) >= price_bracket * fare_bracket_size) \
+                            #make sure theyre ticket range
+                        &   (data[0::,9].astype(np.float) <  (price_bracket + 1) * fare_bracket_size),1]
+                            #put in the second column
+
+            survival_table[0, passenger_class, price_bracket] = np.mean(women_only_stats.astype(np.float))
+            survival_table[1, passenger_class, price_bracket] = np.mean(men_only_stats.astype(np.float))
+
+            survival_table[ survival_table != survival_table] = 0
+
+    survival_table[ survival_table <   0.5 ] = 0        
+    survival_table[ survival_table >=  0.5 ] = 1   
+
+    #print survival_table
+    
+    #have yet to think of a good way to modularize this code
+    test_file = open("test.csv", 'rb')
+    test_file_object = csv.reader(test_file)
+    test_file_header = test_file_object.next()
+
+    prediction_file = open("genderclassticketmodel.csv", 'wb')
+    prediction_file_object = csv.writer(prediction_file)
+    prediction_file_object.writerow(["PassengerId", "Survived"])
+
+
+    for passenger in test_file_object:
+
+        for j in xrange(number_of_price_brackets):
+
+            try:
+                passenger[8] = float(passenger[8])
+            except:
+                bin_fare = 3 - float(passenger[1])
+                break
+
+            if passenger[8] > fare_ceiling:
+                bin_fare = number_of_price_brackets - 1
+                break
+
+            if passenger[8] >= j * fare_bracket_size\
+                and passenger[8] < (j+1) * fare_bracket_size:
+                bin_fare = j
+                break
+
+        if passenger[3] == 'female':
+            prediction_file_object.writerow([passenger[0], 
+                                            "%d" % int( survival_table[0, float(passenger[1]) - 1, bin_fare])])
+        else:
+            
+            prediction_file_object.writerow([passenger[0], 
+                                            "%d" % int( survival_table[1, float(passenger[1]) - 1, bin_fare])])
+
+    #close the files for good
+    test_file.close()
+    prediction_file.close()
+
+    
+
+
 
 def gender_prediction():
 
@@ -68,7 +158,6 @@ def main():
     women_only_stats = data[0::,4] == "female"
     men_only_stats = data[0::,4] == "male"
 
-    #print men_only_stats
     #gets column of Survived from specific rows, 
     men_onboard = data[men_only_stats, 1].astype(np.float)
     women_onboard = data[women_only_stats, 1].astype(np.float)
@@ -79,10 +168,11 @@ def main():
     #print "Proportion of women survivors: {}".format(women_percentage_survived)
     #print "Proportion of men survivors:   {}".format(men_percentage_survived)
 
-    #for i in range(0, 10):
-    #    print data[i]
-
+    #first prediction model only uses gender
     #gender_prediction()
+
+    gender_class_and_ticket_price(header, data)
+
 
 if __name__== "__main__":
     main()
